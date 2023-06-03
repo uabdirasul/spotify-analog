6874cf0e97b47fe8f98bb12e1bcd19cfe110cf49
uabdirasulm <ubbiniyazov04@gmail.com>
2023-06-03 17:57:07 +0500

    add basic component

---
+++ src/components/Player.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faPlay,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start time</p>
+        <input type="range" name="song-range" id="song-range" />
+        <p>End time</p>
+      </div>
+      <div className="player-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>IMG</h1>
+      <h2>Song name</h2>
+      <h3>Song artist</h3>
+    </div>
+  );
+};
+
+export default Song;
