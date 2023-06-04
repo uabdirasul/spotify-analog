08676b00744d21da55fbe43abfd0188968c2552c
uabdirasulm <ubbiniyazov04@gmail.com>
2023-06-04 10:57:01 +0500

    update util.js

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-import { uuid } from "uuidv4";
+import { uuidv4 } from "uuidv4";
 
 function chillHop() {
   return [
@@ -7,7 +7,8 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2023/05/9e6aaa4e04b9e4054f8ed2586d331113e82247da-1024x1024.jpg",
       artist: "J.Folk",
-      id: uuid(),
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=58833",
+      id: uuidv4(),
       active: false,
     },
     {
@@ -15,7 +16,8 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2023/05/5683fa8ae05221c0d06eeedd90fe00e653d27c48-1024x1024.jpg",
       artist: "Yasper",
-      id: uuid(),
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=58833",
+      id: uuidv4(),
       active: "false",
     },
     {
@@ -23,7 +25,8 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2022/11/379d68b8db2ba38393c379a5cdf74ff8b255ae9a-1024x1024.jpg",
       artist: "Misha, cocabona, J.Lamotta",
-      id: uuid(),
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=58833",
+      id: uuidv4(),
       active: false,
     },
     {
@@ -31,7 +34,8 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
       artist: "Shrimpnose",
-      id: uuid(),
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=58833",
+      id: uuidv4(),
       active: false,
     },
     {
@@ -39,7 +43,8 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2022/09/ce636ede9d5fc9fc2417ce40af98060fbcbdadca-1024x1024.jpg",
       artist: "Psalm Trees",
-      id: uuid(),
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=58833",
+      id: uuidv4(),
       active: false,
     },
   ];
