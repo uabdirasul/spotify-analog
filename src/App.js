8c4002050f96c0603165904affc109b0b65a83c8
uabdirasulm <ubbiniyazov04@gmail.com>
2023-06-04 11:41:25 +0500

    add songs and current song to state object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/App.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./util";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
