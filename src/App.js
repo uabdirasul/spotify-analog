783f0246b3ef6cfb3e858a18e7e9d4bfaee92e3f
uabdirasulm <ubbiniyazov04@gmail.com>
2023-06-07 16:10:22 +0500

    add nav component for toggling library style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/App.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import Library from "./components/Library";
+import Nav from "./components/Nav";
 import data from "./util";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     currentTime: 0,
     duration: 0,
   });
+  const [libraryStatus, setLibraryStatus] = useState(false);
 
   const audioRef = useRef(null);
 
@@ -32,6 +34,7 @@ function App() {
 
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
         currentSong={currentSong}
@@ -48,6 +51,7 @@ function App() {
         audioRef={audioRef}
         isPlaying={isPlaying}
         setSongs={setSongs}
+        libraryStatus={libraryStatus}
       />
     </div>
   );
--- src/components/Library.js
+++ src/components/Library.js
@@ -8,9 +8,10 @@ const Library = ({
   isPlaying,
   setIsPlaying,
   setSongs,
+  libraryStatus,
 }) => {
   return (
-    <div className="library">
+    <div className={`library ${libraryStatus ? "active-library" : ""}`}>
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => {
